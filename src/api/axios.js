bc9ba26853cca55643f80574652bc1491f93b16a
amonovdobry07 <amonovogbaek1221@gmail.com>
2026-04-25 10:38:38 +0500

    api added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://127.0.0.1:8000/api'
+const API_URL = 'https://darslik-platforma.onrender.com/api'
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
